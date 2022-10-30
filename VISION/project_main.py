16a355b873e65b37a3db3444ab7c503be7ca77d0
super-cinnamon <yasmine001tlb@gmail.com>
2022-10-30 11:01:09 +0100

    i tried

---
+++ VISION/project_main.py
@@ -0,0 +1,20 @@
+import cv2
+from project_tests import encode, decode
+
+
+input_image = "VISION\supernova.jpg"
+output_image = "encoded_image.PNG"
+secret_data = "This is a top secret message."
+im = cv2.imread("encoded_image.PNG",-1)
+#cv2.imshow("a",im)
+# encode the data into the image
+encoded_image = encode(image_name=input_image, secret_data=secret_data)
+# save the output image (encoded image)
+cv2.imwrite(output_image, encoded_image)
+    
+# decode the secret data from the image
+decoded_data = decode(output_image)
+print("[+] Decoded data:", decoded_data)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
--- VISION/project_tests.py
+++ VISION/project_tests.py
@@ -97,20 +97,20 @@ def decode(image_name):
 
 
 
-if __name__ == "__main__":
-    input_image = "VISION\supernova.jpg"
-    output_image = "encoded_image.PNG"
-    secret_data = "This is a top secret message."
-    im = cv2.imread("encoded_image.PNG",-1)
-    cv2.imshow("a",im)
-    # encode the data into the image
-    encoded_image = encode(image_name=input_image, secret_data=secret_data)
-    # save the output image (encoded image)
-    cv2.imwrite(output_image, encoded_image)
+# if __name__ == "__main__":
+#     input_image = "VISION\supernova.jpg"
+#     output_image = "encoded_image.PNG"
+#     secret_data = "This is a top secret message."
+#     im = cv2.imread("encoded_image.PNG",-1)
+#     cv2.imshow("a",im)
+#     # encode the data into the image
+#     encoded_image = encode(image_name=input_image, secret_data=secret_data)
+#     # save the output image (encoded image)
+#     cv2.imwrite(output_image, encoded_image)
     
-    # # decode the secret data from the image
-    decoded_data = decode(output_image)
-    print("[+] Decoded data:", decoded_data)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+#     # # decode the secret data from the image
+#     decoded_data = decode(output_image)
+#     print("[+] Decoded data:", decoded_data)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
 
